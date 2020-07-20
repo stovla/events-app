c4c3468f71cdb9b6d545be0317e657d4f7a59863
Vlastimir Radojevic <vlastimirsd@gmail.com>
2020-07-20 08:52:34 +0200

    Initial Commit

---
+++ EventsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventsApp
+//
+//  Created by Vlastimir on 20/07/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
